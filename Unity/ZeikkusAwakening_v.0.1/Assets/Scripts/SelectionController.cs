a006c6edef06bdc147eed67a9bac2935973a28fd
ZeikkuSSJ7 <47304153+ZeikkuSSJ7@users.noreply.github.com>
2021-12-27 23:04:42 +0100

    #35 funcionalidad de botones de selección principal

---
+++ Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionController : MonoBehaviour
+{
+
+    public Button newFile, continuee, options, exit;
+
+    public GameObject opciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newFile.onClick.AddListener(() =>
+        {
+            //iniciar juego
+        });
+        continuee.interactable = false;
+        options.onClick.AddListener(() =>
+        {
+            Instantiate(opciones, GameObject.FindGameObjectWithTag("UI").transform);
+            Destroy(gameObject);
+        });
+        exit.onClick.AddListener(() => Application.Quit(0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
