fe7f33bcadfdeae679af0448a187ad1122217116
ZeikkuSSJ7 <47304153+ZeikkuSSJ7@users.noreply.github.com>
2021-12-27 02:44:53 +0100

    Organización de carpetas de UI y #35 pantalla de título terminada

--- Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/TitleScreenManager.cs
+++ Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/TitleScreenManager.cs
@@ -7,6 +7,30 @@ using UnityEngine.UI;
 public class TitleScreenManager : MonoBehaviour
 {
     public Image logobg, logo, titlebg, titlelogo, titlepress;
+    public GameObject selecciones;
+
+    private InputManager iMgr;
+    private PlayerControls playerControls;
+    private bool titlepressActive;
+    private bool titlepressVisible;
+    private Vector2 movementInput;
+    private bool bInput;
+    private bool aInput;
+    
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            playerControls.PlayerActions.B.performed += i => bInput = true;
+            playerControls.PlayerActions.B.canceled += i => bInput = false;
+            playerControls.PlayerActions.A.performed += i => aInput = true;
+            playerControls.PlayerActions.A.canceled += i => aInput = false;
+
+        }
+        playerControls.Enable();
+    }
 
     private void Start()
     {
@@ -32,5 +56,36 @@ public class TitleScreenManager : MonoBehaviour
         titlelogo.CrossFadeAlpha(1, 1, true);
         yield return new WaitForSeconds(1f);
         titlepress.CrossFadeAlpha(1, 1, true);
+        yield return new WaitForSeconds(1f);
+        titlepressActive = true;
+        titlepressVisible = true;
+        StartCoroutine(AnimatePressStart());
+    }
+    
+    IEnumerator AnimatePressStart()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.5f);
+            if (titlepressActive)
+            {
+                titlepress.CrossFadeAlpha(titlepressVisible ? 0 : 1, 2, true);
+                titlepressVisible = !titlepressVisible;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (titlepressActive)
+            if (aInput)
+            {
+                aInput = false;
+                titlepressActive = false;
+                titlepressVisible = false;
+                selecciones.SetActive(true);
+                titlepress.enabled = false;
+            }
+                
     }
 }
