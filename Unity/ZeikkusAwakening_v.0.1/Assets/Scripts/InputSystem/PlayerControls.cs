65d471ae182c55edbb1c92f94e274394a3562dee
mirirore <miridogsteam@gmail.com>
2022-01-02 19:46:28 +0100

    escenario y VFX #23
    - laberinto plantas 1 y 2 con objetos
    - Efecto niebla
    - Importado Posible efecto portal final
    
    Falta unificar efectos visuales con escenario e iluminacion

--- Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/InputSystem/PlayerControls.cs
+++ Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/InputSystem/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputSystem/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
