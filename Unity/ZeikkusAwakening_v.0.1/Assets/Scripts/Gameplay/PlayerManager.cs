18ef3d08b61dc3280d2e99d8ff7a66a8ec9e9621
Jorge Sánchez <47304153+ZeikkuSSJ7@users.noreply.github.com>
2022-01-31 21:58:05 +0100

    #43 inicio de colocacion de luces

--- Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/Gameplay/PlayerManager.cs
+++ Unity/ZeikkusAwakening_v.0.1/Assets/Scripts/Gameplay/PlayerManager.cs
@@ -35,7 +35,7 @@ public class PlayerManager : MonoBehaviour
     private void LateUpdate()
     {
         isInteracting = animator.GetBool("isInteracting");
-        isUsingRootMotion = animator.GetBool("isUsingRootMotion");
+        //isUsingRootMotion = animator.GetBool("isUsingRootMotion");
         if (gameManager.inWorld)
         {
             playerLocomotion.isJumping = animator.GetBool("isJumping");
