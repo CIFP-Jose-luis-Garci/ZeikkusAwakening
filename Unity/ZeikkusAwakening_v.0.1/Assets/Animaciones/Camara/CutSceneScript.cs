81b2e5966a355168d0344c8f6a90813e76f69814
mirirore <miridogsteam@gmail.com>
2022-02-10 18:41:03 +0100

    camara cinematica
    - en proceso

--- Unity/ZeikkusAwakening_v.0.1/Assets/Animaciones/Camara/CutSceneScript.cs
+++ Unity/ZeikkusAwakening_v.0.1/Assets/Animaciones/Camara/CutSceneScript.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using Cinemachine;
 
 public class CutSceneScript : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera mainFollowVCam, bossVcam;
+    private PlayableDirector director;
+    
+
+
+    
+    //[SerializeField] private CinemachineVirtualCamera mainFollowVCam, bossVcam;
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            bossVcam.Priority = mainFollowVCam.Priority + 1;
-
-        }
-    }
+     {
+         if (other.CompareTag("Player"))
+         {
+            // bossVcam.Priority = mainFollowVCam.Priority + 1;
+            director.Play();
+         }
+     }
 
     /*private void OnTriggerExit()
     {
         bossVcam.Priority = mainFollowVCam.Priority - 1;
-    }*/
+    }
 
     /* Start is called before the first frame update
     void Start()
@@ -32,5 +38,5 @@ public class CutSceneScript : MonoBehaviour
     {
         
     }*/
-    
+
 }
